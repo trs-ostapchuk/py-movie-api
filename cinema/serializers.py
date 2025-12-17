21cc24e89e4ec63e1d2ce173a605d6ae65a2ddc9
Taras <tarasostapchuk27@gmail.com>
2025-12-17 17:41:12 +0100

    Add MovieSerializer

---
+++ cinema/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from cinema.models import Movie
+
+
+class MovieSerializers(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(required=True, max_length=255)
+    description = serializers.CharField()
+    duration = serializers.IntegerField(required=True)
+
+    def create(self, validated_data):
+        """
+        Create and return a new `Movie` instance, given the validated data.
+        """
+        return Movie.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        """
+        Update and return an existing `Movie` instance, given the validated data.
+        """
+        instance.title = validated_data.get("title", instance.title)
+        instance.description = validated_data.get("description", instance.description)
+        instance.duration = validated_data.get("duration", instance.duration)
+        instance.save()
+        return instance
